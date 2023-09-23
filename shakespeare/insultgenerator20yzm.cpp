d4892b17d499f6da906eafd433f8c403839d6791
Bleedem <jjacobwow@gmail.com>
2023-09-23 15:05:33 -0400

    creation

---
+++ shakespeare/insultgenerator20yzm.cpp
@@ -0,0 +1,18 @@
+/*
+ * insultgenerator20yzm.cpp
+ *
+ *  Created on: Sep. 20, 2023
+ *      Author: jjaco
+ */
+
+#include "insultgenerator20yzm.h"
+
+insultgenerator_20yzm::insultgenerator_20yzm() {
+	// TODO Auto-generated constructor stub
+
+}
+
+insultgenerator_20yzm::~insultgenerator_20yzm() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ shakespeare/insultgenerator20yzm.h
@@ -0,0 +1,17 @@
+/*
+ * insultgenerator20yzm.h
+ *
+ *  Created on: Sep. 20, 2023
+ *      Author: jjaco
+ */
+
+#ifndef INSULTGENERATOR20YZM_H_
+#define INSULTGENERATOR20YZM_H_
+
+class insultgenerator_20yzm {
+public:
+	insultgenerator_20yzm();
+	virtual ~insultgenerator_20yzm();
+};
+
+#endif /* INSULTGENERATOR20YZM_H_ */
